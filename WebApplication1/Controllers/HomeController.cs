a88992502516ec95c5169b985956386d2deae75a
waqar <waqar@DESKTOP-9FIJ5FI>
2019-05-24 19:56:23 +0500

    ee

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var aa = "test";
+            aa = "test";
+            aa = "test";
+            aa = "test";
+            aa = "test";
+            aa = "test";
+            aa = "test";
+            aa = "test";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+       
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
